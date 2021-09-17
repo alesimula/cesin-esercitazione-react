5af3d3a0a0a528e0f9d9f32b381ecacd22ceda7e
mauro <mauro.dallago@libero.it>
2021-09-17 14:46:46 +0200

    aggiornato

--- src/index.js
+++ src/index.js
@@ -4,14 +4,52 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+class Clienti extends React.Component {
+
+  getClienti() {
+
+    let clienti=[];
+
+   for(var i=0;i<10;i++){
+     clienti.push(
+      <tr>
+         <th class="text-center">1</th>
+         <th class="text-center">MarioRossi</th>
+         <th class="text-center">24 via roma</th>
+         <th class="text-center"><input type="checkbox" checked='true' /></th>
+         <th class="text-center"><div><i class="bi bi-pencil-square btn btn-info"></i></div></th>
+         <th class="text-center"><div><i class="bi bi-trash btn btn-danger"></i></div></th>
+      </tr>
+     )
+   }
+     
+   return clienti;
+  }
+
+  render() {
+    return(
+      <div>
+        <table class="table">
+          <tr class="thead-dark">
+            <th class="text-center">ID</th>
+            <th class="text-center">Name</th>
+            <th class="text-center">Address</th>
+            <th class="text-center">Public</th>
+            <th class="text-center">edit</th>
+            <th class="text-center">delete</th>
+          </tr>
+          {this.getClienti()}
+
+        </table>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Clienti />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
