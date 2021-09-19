f65a61fb92cf7cffe0f2015d2325d6b55dccfe48
Alex Sim <ale.simula97@gmail.com>
2021-09-19 19:24:22 +0200

    Usa td anzichè th

--- src/index.js
+++ src/index.js
@@ -26,18 +26,18 @@ class Clienti extends React.Component {
         .then(result => {
             let clienti = result.data.map(cliente => (
               <tr>
-                <th class="text-center">{cliente.id}</th>
-                <th class="text-center">{cliente.name}</th>
-                <th class="text-center">{cliente.address}</th>
-                <th class="text-center">
+                <td class="text-center">{cliente.id}</td>
+                <td class="text-center">{cliente.name}</td>
+                <td class="text-center">{cliente.address}</td>
+                <td class="text-center">
                   <div class="custom-control custom-checkbox">
                     <label class="sr-only position-static"></label>
                     <input class="custom-control-input" type="checkbox" checked={cliente.public ? true : false}/>
                     <span class="custom-control-label"></span>
                   </div>
-                </th>
-                <th class="text-center"><Link to={`/edit/${cliente.id}`}><i class="bi bi-pencil-square btn btn-info"></i></Link></th>
-                <th class="text-center"><Link to={`/remove/${cliente.id}`}><i class="bi bi-trash btn btn-danger"></i></Link></th>
+                </td>
+                <td class="text-center"><Link to={`/edit/${cliente.id}`}><i class="bi bi-pencil-square btn btn-info"></i></Link></td>
+                <td class="text-center"><Link to={`/remove/${cliente.id}`}><i class="bi bi-trash btn btn-danger"></i></Link></td>
               </tr>
             ))
             self.setState({clienti: clienti})
